a35481887eb288cd4246e12a2ceabff3b4b80061
yanbo0039 <yanbo0039@yeah.net>
2019-08-29 19:41:56 +0800

    doGet()方法。。。

--- src/net/tencent/my12306/controller/LifeServlet.java
+++ src/net/tencent/my12306/controller/LifeServlet.java
@@ -25,8 +25,7 @@ public class LifeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet()方法。。。");
 	}
 
 	/**
