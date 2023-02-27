a8b3a196d336538cfb3a85df3445c81a9e02186f
yanbo0039 <yanbo0039@yeah.net>
2023-02-27 09:58:22 +0800

    servlet

---
+++ src/main/java/net/tencent/tickets/controller/ToUpdateUserServlet.java
@@ -0,0 +1,62 @@
+package net.tencent.tickets.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.tencent.tickets.entity.Users;
+import net.tencent.tickets.service.CityService;
+import net.tencent.tickets.service.ProvinceService;
+import net.tencent.tickets.service.UserService;
+
+/**
+ * 
+* <p>Title: ToUpdateUserServlet</p>  
+* <p>
+*	Description: 
+* 去往用户修改信息页面的servlet
+* 需要获取以下信息
+* 1.用户本人信息
+* 2.所有省份信息
+* 3.获取当前用户所在省份的所有城市信息
+* </p> 
+* @author xianxian 
+* @date 2019年9月4日
+ */
+@WebServlet("/ToUpdateUserServlet")
+public class ToUpdateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//先拿用户信息
+		
+		//跳转到编辑页面修改用户信息
+		
+		//1.借助session，拿username和password，然后访问数据库获取用户的完整信息
+				HttpSession session=request.getSession();
+				Users user = (Users)session.getAttribute("user");
+				
+				Users result = UserService.getInstance().login(user.getUserName(), user.getUserPassword());
+				
+				
+				
+				//2.把用户信息传给页面，并跳转到目标页面
+				request.setAttribute("userinfo", result);
+				//获取所有省份并传给页面
+				request.setAttribute("provinces", ProvinceService.getInstance().getAllProvince());
+				//获取当前用户所在省份的所有城市信息并传给页面
+				request.setAttribute("cities", CityService.getInstance().getCityByProvinceid
+						(result.getCity().getProvince().getProvinceNum()));
+				
+				request.getRequestDispatcher("/user/userinfo_edit.jsp").forward(request, response);
+	}
+	
+	
+
+}
---
+++ src/main/java/net/tencent/tickets/controller/UpdatePasswordServlet.java
@@ -0,0 +1,70 @@
+package net.tencent.tickets.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.tencent.tickets.entity.Users;
+import net.tencent.tickets.service.UserService;
+import net.tencent.tickets.util.Md5Utils;
+
+/**
+ * 去往修改密码页面的servlet
+ */
+@WebServlet("/UpdatePasswordServlet")
+public class UpdatePasswordServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		
+		String p_old = request.getParameter("password_old");
+		String p_new = request.getParameter("password_new");
+		String p_new_confirm = request.getParameter("password_new_confirm");
+		
+		HttpSession session=request.getSession();
+		Users user = (Users) session.getAttribute("user");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		
+		
+		//请大家把服务端校验补全，明天早上检查
+		if(p_new_confirm.equals(p_new) == true) {
+			/*
+			 * 更新密码的思路：
+			 * 1.查看原密码是否正确
+			 * 2.如果正确，那么更新密码
+			 */
+		
+			
+			//3.写三行代码ok
+			//user.getPassword().equals(Md5Utils.md5(p_old))
+			if(UserService.getInstance().updatePassword(user.getId(), Md5Utils.md5(p_old), Md5Utils.md5(p_new)))
+			{
+				pw.println("<script>alert('更新密码成功,请重新登录');window.open('ExitServlet','_parent');</script>");
+				
+				//response.sendRedirect("ExitServlet");
+			}else
+			{
+//				pw.println("<script>alert('更新密码失败,请稍后再试。');</script>");
+				request.setAttribute("mes_alert", "更新密码失败,请稍后再试。");
+				request.getRequestDispatcher("/user/user_password_edit.jsp").forward(request, response);
+			}
+		}else {
+			request.setAttribute("mes_alert", "确认新密码与新密码不一至,请重新输入。");
+			request.getRequestDispatcher("/user/user_password_edit.jsp").forward(request, response);
+		}
+		
+	}
+   
+}
